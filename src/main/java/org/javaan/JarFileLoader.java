f31456f4c1407c00a2289c2f758a4fdf1078a680
andreas <andreas@bla.de>
2013-10-17 18:14:36 +0200

    implemented processing of archives which contain archives, like war and ear files.

--- src/main/java/org/javaan/JarFileLoader.java
+++ src/main/java/org/javaan/JarFileLoader.java
@@ -2,16 +2,66 @@ package org.javaan;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.Random;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
 import org.apache.bcel.classfile.ClassParser;
 import org.apache.bcel.classfile.JavaClass;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 public class JarFileLoader {
+	
+	private void processJar(InputStream input, List<JavaClass> classes) throws IOException {
+		File file = File.createTempFile(new Random().nextLong() + "", ".jar");
+		OutputStream output = FileUtils.openOutputStream(file);
+		try {
+			IOUtils.copy(input, output);
+		} finally {
+			output.close();
+		}
+		try {
+			processJar(file.getAbsolutePath(), new JarFile(file), classes);
+		} finally {
+			file.delete();
+		}
+	}
+	
+	private void processEntry(String fileName, JarFile jar, List<JavaClass> classes, JarEntry entry) throws IOException {
+		if (!entry.isDirectory()) {
+			String name = entry.getName();
+			boolean isClass = name.endsWith(".class");
+			boolean isLibrary = name.endsWith(".jar") || name.endsWith(".war") || name.endsWith(".ear");
+			if (isClass) {
+				ClassParser parser = new ClassParser(fileName, entry.getName());
+				classes.add(parser.parse());
+			} else if (isLibrary) {
+				InputStream input = jar.getInputStream(entry);
+				try {
+					processJar(input, classes);
+				} finally {
+					input.close();
+				}
+			}
+		}
+	}
+	
+	private void processJar(String fileName, JarFile jar, List<JavaClass> classes) throws IOException {
+		try {
+			Enumeration<JarEntry> entries = jar.entries();
+			while (entries.hasMoreElements()) {
+				processEntry(fileName, jar, classes, entries.nextElement());
+			}
+		} finally {
+			jar.close();
+		}
+	}
 
 	public List<JavaClass> loadJavaClasses(String[] fileNames)
 			throws IOException {
@@ -23,18 +73,7 @@ public class JarFileLoader {
 						"JAR file %s does not exist", fileName));
 			}
 			JarFile jar = new JarFile(file);
-			try {
-				Enumeration<JarEntry> entries = jar.entries();
-				while (entries.hasMoreElements()) {
-					JarEntry entry = entries.nextElement();
-					if (!entry.isDirectory() && entry.getName().endsWith(".class")) {
-						ClassParser parser = new ClassParser(fileName, entry.getName());
-						classes.add(parser.parse());
-					}
-				}
-			} finally {
-				jar.close();
-			}
+			processJar(fileName, jar, classes);
 		}
 
 		return classes;
