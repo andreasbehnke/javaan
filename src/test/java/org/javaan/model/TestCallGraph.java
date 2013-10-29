1834d68387476c7a9d05ae6c99ab334de7f41528
andreas <andreas@bla.de>
2013-10-29 17:19:54 +0100

    removed unused imports

--- src/test/java/org/javaan/model/TestCallGraph.java
+++ src/test/java/org/javaan/model/TestCallGraph.java
@@ -3,11 +3,11 @@ package org.javaan.model;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
 
 import java.util.Set;
 
-import org.javaan.graph.Visitor;
 import org.junit.Test;
 import org.mockito.InOrder;
 
