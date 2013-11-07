685b92741f3179d4b21f7684c9c29d1bc6aedd0d
andreas <andreas@bla.de>
2013-11-07 08:08:30 +0100

    made minor changes to wording and removed unused option

--- src/main/java/org/javaan/commands/StandardOptions.java
+++ src/main/java/org/javaan/commands/StandardOptions.java
@@ -18,19 +18,17 @@ public interface StandardOptions {
 	
 	static final String OPT_FILTER = "f";
 	
-	static final String OPT_METHOD = "method";
-	
 	static final String OPT_CALLERS = "callers";
 	
-	static final Option SUPER_TYPES = new Option(StandardOptions.OPT_SUPER, "superTypes", false, "For each type list the hierachy of super types");
+	static final Option SUPER_TYPES = new Option(StandardOptions.OPT_SUPER, "super", false, "For each type, list the hierachy of super types");
 	
-	static final Option SPECIALIZATIONS = new Option(StandardOptions.OPT_SPECIALIZATIONS, "specializations", false, "For each type list specialization types");
+	static final Option SPECIALIZATIONS = new Option(StandardOptions.OPT_SPECIALIZATIONS, "specializations", false, "For each type, list specialization types");
 	
-	static final Option INTERFACES = new Option(StandardOptions.OPT_INTERFACES, "interfaces", false, "For each class list all implemented interfaces");
+	static final Option INTERFACES = new Option(StandardOptions.OPT_INTERFACES, "interfaces", false, "For each class, list all implemented interfaces");
 	
-	static final Option METHODS = new Option(StandardOptions.OPT_METHODS, "methods", false, "For each type list methods");
+	static final Option METHODS = new Option(StandardOptions.OPT_METHODS, "methods", false, "For each type, list methods");
 	
-	static final Option VIRTUAL_METHODS = new Option(StandardOptions.OPT_VIRTUAL_METHODS, "virtualMethods", false, "For each type list all methods and all inherited (virtual) methods");
+	static final Option VIRTUAL_METHODS = new Option(StandardOptions.OPT_VIRTUAL_METHODS, "virtualMethods", false, "For each type, list all methods and all inherited (virtual) methods");
 
 	static final Option FILTER = new Option(StandardOptions.OPT_FILTER, "filter", true, "Filter types by <arg>. All types which contain <arg> in name will match.");
 
