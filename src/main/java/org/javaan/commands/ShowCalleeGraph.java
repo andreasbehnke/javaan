d6e4df5d9310d6efdd279413c3eb9ec84f2879f3
andreas <andreas@bla.de>
2013-11-12 08:26:50 +0100

    implemented command callee

--- src/main/java/org/javaan/commands/ShowCalleeGraph.java
+++ src/main/java/org/javaan/commands/ShowCalleeGraph.java
@@ -4,11 +4,11 @@ import org.javaan.model.CallGraph;
 import org.javaan.model.Method;
 import org.javaan.print.GraphPrinter;
 
-public class ShowCallerGraph extends BaseCallGraphCommand {
+public class ShowCalleeGraph extends BaseCallGraphCommand {
 
-	private final static String NAME = "callers";
+	private final static String NAME = "callee";
 
-	private final static String DESCRIPTION = "Display the graph of methods which call methods defined with option method.";
+	private final static String DESCRIPTION = "Display the graph of methods being called by methods defined with option method.";
 
 	@Override
 	public String getName() {
@@ -22,7 +22,7 @@ public class ShowCallerGraph extends BaseCallGraphCommand {
 
 	@Override
 	protected void traverse(CallGraph callGraph, Method method, int maxDepth, GraphPrinter<Method> graphPrinter) {
-		callGraph.traverseCallers(method, -1, graphPrinter);
+		callGraph.traverseCallees(method, -1, graphPrinter);
 	}
 
 }
