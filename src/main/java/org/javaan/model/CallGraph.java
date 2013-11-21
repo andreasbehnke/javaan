772b1f225f54e7b539dc9ecb76d77105e3e07914
andreas <andreas@bla.de>
2013-11-21 21:32:16 +0100

    implemented used types, using types and dependency cycle of types for CallGraph

--- src/main/java/org/javaan/model/CallGraph.java
+++ src/main/java/org/javaan/model/CallGraph.java
@@ -1,18 +1,23 @@
 package org.javaan.model;
 
+import java.util.List;
 import java.util.Set;
 
 import org.javaan.graph.NamedObjectDirectedGraph;
+import org.javaan.graph.NamedObjectEdge;
 import org.javaan.graph.NamedObjectVisitor;
+import org.jgrapht.alg.StrongConnectivityInspector;
 
 /**
- * Represents the call-graph of all loaded methods. Leaf nodes of the graph represent potential 
- * entry points to loaded libraries.
+ * Represents the call-graph of all loaded methods.
+ * Type dependencies are created for every method.
  */
 public class CallGraph {
 	
 	private final NamedObjectDirectedGraph<Method> callerOfMethod = new NamedObjectDirectedGraph<Method>();
-	
+
+	private final NamedObjectDirectedGraph<Type> usageOfType = new NamedObjectDirectedGraph<Type>();
+
 	public int size() {
 		return callerOfMethod.vertexSet().size();
 	}
@@ -25,6 +30,7 @@ public class CallGraph {
 			throw new IllegalArgumentException("Parameter callee must not be null");
 		}
 		callerOfMethod.addEdge(caller, callee);
+		usageOfType.addEdge(caller.getType(), callee.getType());
 	}
 	
 	public Set<Method> getCallers(Method callee) {
@@ -49,5 +55,21 @@ public class CallGraph {
 	
 	public Set<Method> getLeafCallees(Method caller) {
 		return callerOfMethod.getLeafSuccessors(caller);
+	}
+
+	public void traverseUsedTypes(Type using, NamedObjectVisitor<Type> usedVisitor) {
+		usageOfType.traverseSuccessorsDepthFirst(using, usedVisitor);
+	}
+	
+	public void traverseUsingTypes(Type used, NamedObjectVisitor<Type> usingVisitor) {
+		usageOfType.traversePredecessorsDepthFirst(used, usingVisitor);
+	}
+
+	/**
+	 * @return list of type sets which take part in a using dependency cycle
+	 */
+	public List<Set<Type>> getDependencyCycles() {
+		StrongConnectivityInspector<Type, NamedObjectEdge<Type>> inspector = new StrongConnectivityInspector<Type, NamedObjectEdge<Type>>(usageOfType);
+		return inspector.stronglyConnectedSets();
 	}
 }
