64ae8a7315bec8e32da102a080d572f76505c7e6
Andreas Behnke <ab@andreasbehnke.com>
2014-01-07 00:10:36 +0100

    display simple example graph

--- codeforest/src/main/java/org/codeforest/CodeForest.java
+++ codeforest/src/main/java/org/codeforest/CodeForest.java
@@ -1,171 +1,181 @@
-/*
- * $RCSfile$
- *
- * Copyright (c) 2007 Sun Microsystems, Inc. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * - Redistribution of source code must retain the above copyright
- *   notice, this list of conditions and the following disclaimer.
- *
- * - Redistribution in binary form must reproduce the above copyright
- *   notice, this list of conditions and the following disclaimer in
- *   the documentation and/or other materials provided with the
- *   distribution.
- *
- * Neither the name of Sun Microsystems, Inc. or the names of
- * contributors may be used to endorse or promote products derived
- * from this software without specific prior written permission.
- *
- * This software is provided "AS IS," without a warranty of any
- * kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND
- * WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY
- * EXCLUDED. SUN MICROSYSTEMS, INC. ("SUN") AND ITS LICENSORS SHALL
- * NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF
- * USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS
- * DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE FOR
- * ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL,
- * CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND
- * REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR
- * INABILITY TO USE THIS SOFTWARE, EVEN IF SUN HAS BEEN ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGES.
- *
- * You acknowledge that this software is not designed, licensed or
- * intended for use in the design, construction, operation or
- * maintenance of any nuclear facility.
- *
- * $Revision$
- * $Date$
- * $State$
- */
-
 package org.codeforest;
 
 import com.sun.j3d.utils.universe.*;
+import com.sun.j3d.utils.behaviors.vp.OrbitBehavior;
 import com.sun.j3d.utils.geometry.ColorCube;
+
 import javax.media.j3d.*;
 import javax.vecmath.*;
+
+import org.codeforest.graph.NodeFactory;
+import org.codeforest.graph.VertexTreeSceneBuilder;
+import org.codeforest.model.VertexSceneContext;
+import org.jgrapht.DirectedGraph;
+import org.jgrapht.EdgeFactory;
+import org.jgrapht.graph.DefaultDirectedGraph;
+
 import java.awt.GraphicsConfiguration;
 
 /**
- * Simple Java 3D example program to display a spinning cube.
+ * First test: Display a simple JGraph
  */
 public class CodeForest extends javax.swing.JFrame {
 
-    private static final long serialVersionUID = 1L;
-
-    private SimpleUniverse univ = null;
+	private static final long serialVersionUID = 1L;
+	
+	private static final String A = "A";
+	private static final String B = "B";
+	private static final String C = "C";
+	private static final String D = "D";
+	private static final String E = "E";
+	private static final String F = "F";
+	private static final String G = "G";
+	private static final String H = "H";
+	private static final String I = "I";
+	private static final String J = "J";
+	private static final String K = "K";
+	private static final String L = "L";
+	private static final String M = "M";
+	private static final String N = "N";
+	private static final String O = "O";
+	
+	private SimpleUniverse univ = null;
     private BranchGroup scene = null;
+    
+    private javax.swing.JPanel drawingPanel;
 
-    public BranchGroup createSceneGraph() {
-	// Create the root of the branch graph
-	BranchGroup objRoot = new BranchGroup();
-
-	// Create the TransformGroup node and initialize it to the
-	// identity. Enable the TRANSFORM_WRITE capability so that
-	// our behavior code can modify it at run time. Add it to
-	// the root of the subgraph.
-	TransformGroup objTrans = new TransformGroup();
-	objTrans.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
-	objRoot.addChild(objTrans);
-
-	// Create a simple Shape3D node; add it to the scene graph.
-	objTrans.addChild(new ColorCube(0.4));
-
-	// Create a new Behavior object that will perform the
-	// desired operation on the specified transform and add
-	// it into the scene graph.
-	Transform3D yAxis = new Transform3D();
-	Alpha rotationAlpha = new Alpha(-1, 4000);
-
-	RotationInterpolator rotator =
-	    new RotationInterpolator(rotationAlpha, objTrans, yAxis,
-				     0.0f, (float) Math.PI*2.0f);
-	BoundingSphere bounds =
-	    new BoundingSphere(new Point3d(0.0,0.0,0.0), 100.0);
-	rotator.setSchedulingBounds(bounds);
-	objRoot.addChild(rotator);
-
-        // Have Java 3D perform optimizations on this scene graph.
-        objRoot.compile();
-
-	return objRoot;
-    }
-
-    private Canvas3D createUniverse() {
-	// Get the preferred graphics configuration for the default screen
-	GraphicsConfiguration config =
-	    SimpleUniverse.getPreferredConfiguration();
-
-	// Create a Canvas3D using the preferred configuration
-	Canvas3D c = new Canvas3D(config);
-
-	// Create simple universe with view branch
-	univ = new SimpleUniverse(c);
-
-	// This will move the ViewPlatform back a bit so the
-	// objects in the scene can be viewed.
-	univ.getViewingPlatform().setNominalViewingTransform();
-
-	// Ensure at least 5 msec per frame (i.e., < 200Hz)
-	univ.getViewer().getView().setMinimumFrameCycleTime(5);
-
-	return c;
-    }
-
+    
     /**
      * Creates new form HelloUniverse
      */
     public CodeForest() {
-	// Initialize the GUI components
-	initComponents();
+         // Initialize the GUI components
+         initComponents();
 
-	// Create Canvas3D and SimpleUniverse; add canvas to drawing panel
-	Canvas3D c = createUniverse();
-	drawingPanel.add(c, java.awt.BorderLayout.CENTER);
+         // Create Canvas3D and SimpleUniverse; add canvas to drawing panel
+         Canvas3D c = createUniverse();
+         drawingPanel.add(c, java.awt.BorderLayout.CENTER);
 
-	// Create the content branch and add it to the universe
-	scene = createSceneGraph();
-	univ.addBranchGraph(scene);
+         // Create the content branch and add it to the universe
+         scene = createSceneGraph();
+         univ.addBranchGraph(scene);
+
+         OrbitBehavior orbit = new OrbitBehavior(c);
+         orbit.setSchedulingBounds(new BoundingSphere(new Point3d(0.0, 0.0, 0.0), Double.MAX_VALUE));
+         univ.getViewingPlatform().setViewPlatformBehavior(orbit);
+    }
+    
+    
+    public BranchGroup createSceneGraph() {
+         // Create the root of the branch graph
+         BranchGroup objRoot = new BranchGroup();
+         
+         DirectedGraph<String, String> graph = new DefaultDirectedGraph<String, String>(new EdgeFactory<String, String>() {
+ 			public String createEdge(String sourceVertex, String targetVertex) {
+ 				return sourceVertex + targetVertex;
+ 			};
+ 		});
+ 		graph.addVertex(A);
+ 		graph.addVertex(B);
+ 		graph.addVertex(C);
+ 		graph.addVertex(D);
+ 		graph.addVertex(E);
+ 		graph.addVertex(F);
+ 		graph.addVertex(G);
+ 		graph.addVertex(H);
+ 		graph.addVertex(I);
+ 		graph.addVertex(J);
+ 		graph.addVertex(K);
+ 		graph.addVertex(L);
+ 		graph.addVertex(M);
+ 		graph.addVertex(N);
+ 		graph.addVertex(O);
+ 		graph.addEdge(A, B);
+ 		graph.addEdge(A, C);
+ 		graph.addEdge(B, D);
+ 		graph.addEdge(B, E);
+ 		graph.addEdge(B, F);
+ 		graph.addEdge(C, G);
+ 		graph.addEdge(D, H);
+ 		graph.addEdge(D, I);
+ 		graph.addEdge(D, J);
+ 		graph.addEdge(E, K);
+ 		graph.addEdge(E, L);
+ 		graph.addEdge(F, M);
+ 		graph.addEdge(F, N);
+ 		graph.addEdge(G, O);
+ 		
+ 		NodeFactory<String> shapeFactory = new NodeFactory<String>() {
+ 			public Node createNode(String vertex) {
+ 				return new ColorCube(0.4);
+ 			}
+ 		};
+ 		VertexSceneContext<String> context = new VertexSceneContext<String>();
+ 		VertexTreeSceneBuilder<String, String> sceneBuilder = new VertexTreeSceneBuilder<String, String>(context, graph, shapeFactory, 2d, 3d);
+ 		TransformGroup transformGroup = sceneBuilder.createScene(A);
+ 		Bounds bounds = transformGroup.getBounds();
+ 		
+ 		
+ 		objRoot.addChild(transformGroup);
+
+         // Have Java 3D perform optimizations on this scene graph.
+         objRoot.compile();
+
+         return objRoot;
     }
 
-    // ----------------------------------------------------------------
     
-    /** This method is called from within the constructor to
-     * initialize the form.
-     * WARNING: Do NOT modify this code. The content of this method is
-     * always regenerated by the Form Editor.
-     */
-    // <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:initComponents
+    private Canvas3D createUniverse() {
+         // Get the preferred graphics configuration for the default screen
+         GraphicsConfiguration config = SimpleUniverse.getPreferredConfiguration();
+
+         // Create a Canvas3D using the preferred configuration
+         Canvas3D c = new Canvas3D(config);
+
+         // Create simple universe with view branch
+         univ = new SimpleUniverse(c);
+
+         // This will move the ViewPlatform back a bit so the
+         // objects in the scene can be viewed.
+         ViewingPlatform viewingPlatform = univ.getViewingPlatform();
+         
+         TransformGroup viewTransform = viewingPlatform.getViewPlatformTransform();
+         Transform3D t3d = new Transform3D();
+         t3d.lookAt(new Point3d(0,0,150), new Point3d(0,0,0), new Vector3d(0,1,0));
+         t3d.invert();
+         viewTransform.setTransform(t3d);
+         
+         View view = univ.getViewer().getView(); 
+         view.setBackClipDistance(100.0);
+         view.setMinimumFrameCycleTime(5);
+
+         return c;
+    }
+
+    
     private void initComponents() {
-        drawingPanel = new javax.swing.JPanel();
+         drawingPanel = new javax.swing.JPanel();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("HelloUniverse");
-        drawingPanel.setLayout(new java.awt.BorderLayout());
+         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+         setTitle("HelloUniverse");
+         drawingPanel.setLayout(new java.awt.BorderLayout());
 
-        drawingPanel.setPreferredSize(new java.awt.Dimension(250, 250));
-        getContentPane().add(drawingPanel, java.awt.BorderLayout.CENTER);
+         drawingPanel.setPreferredSize(new java.awt.Dimension(500, 500));
+         getContentPane().add(drawingPanel, java.awt.BorderLayout.CENTER);
 
-        pack();
-    }// </editor-fold>//GEN-END:initComponents
+         pack();
+    }
+    
     
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new CodeForest().setVisible(true);
-            }
-        });
+         java.awt.EventQueue.invokeLater(new Runnable() {
+              public void run() {
+                   new CodeForest().setVisible(true);
+              }
+         });
     }
-    
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel drawingPanel;
-    // End of variables declaration//GEN-END:variables
     
 }
