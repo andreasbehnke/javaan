f3d81ea21c011c6a6bed76993f600a40c340f2d4
Andreas Behnke <ab@andreasbehnke.com>
2014-01-10 08:29:43 +0100

    code formatting

--- codeforest/src/main/java/org/codeforest/scenegraph/LineEdgeFactory.java
+++ codeforest/src/main/java/org/codeforest/scenegraph/LineEdgeFactory.java
@@ -9,6 +9,7 @@ import javax.media.j3d.Shape3D;
 import javax.vecmath.Vector3d;
 
 public class LineEdgeFactory<V, E> implements EdgeNodeFactory<V, E> {
+
 	public Node createNode(E edge, V source, V target, Vector3d startVector, Vector3d endVector) {
 		LineArray lines = new LineArray(2, GeometryArray.COORDINATES);
 		lines.setCoordinates(0, new double[] {
