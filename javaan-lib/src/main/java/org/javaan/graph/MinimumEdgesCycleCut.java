268e2749340bd6fb19d128f1c02a979e73715a6d
Andreas Behnke <ab@andreasbehnke.com>
2014-01-28 18:51:20 +0100

    added algorithm for finding cut point for cycles in directed pseudograph

---
+++ javaan-lib/src/main/java/org/javaan/graph/MinimumEdgesCycleCut.java
@@ -0,0 +1,64 @@
+package org.javaan.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jgrapht.alg.cycle.DirectedSimpleCycles;
+import org.jgrapht.alg.cycle.TiernanSimpleCycles;
+import org.jgrapht.graph.DirectedMultigraph;
+import org.jgrapht.graph.DirectedPseudograph;
+
+/**
+ * Given a directed <a href="http://mathworld.wolfram.com/Pseudograph.html">pseudograph</a> g, 
+ * this algorithm produces a directed acyclic graph g' by breaking cycles between vertices with 
+ * minimum number of edges between them
+ */
+public class MinimumEdgesCycleCut<V, E> {
+
+	private final DirectedPseudograph<V, E> sourceGraph;
+	
+	private final DirectedMultigraph<V, E> targetGraph;
+
+	public MinimumEdgesCycleCut(DirectedPseudograph<V, E> source, DirectedMultigraph<V, E> target) {
+		this.sourceGraph = source;
+		this.targetGraph = target;
+	}
+	
+	private DirectedSimpleCycles<V, E> createCycleDetector() {
+		// Tiernan's algorithm is the only implementation of DirectedSimpleCycles,
+		// which can handle DirectedPseudographs. All other implementations return duplicate
+		// cycles if two vertices have more than one edge connecting them.
+		return new TiernanSimpleCycles<>(sourceGraph);
+	}
+	
+	private CutPoint<V, E> findCutPoint(List<V> cycle) {
+		V cutPointSource = null;
+		V cutPointTarget = null;
+		int minEdgeCount = Integer.MAX_VALUE;
+		V source = cycle.get(cycle.size() - 1);
+		for(int i=0; i < cycle.size(); i++) {
+			V target = cycle.get(i);
+			int edgeCount = sourceGraph.getAllEdges(source, target).size();
+			if (edgeCount < minEdgeCount) {
+				cutPointSource = source;
+				cutPointTarget = target;
+				minEdgeCount = edgeCount;
+			}
+			source = target;
+		}
+		return new CutPoint<V, E>(cutPointSource, cutPointTarget);
+	}
+
+	public List<CutPoint<V, E>> findCutPoints() {
+		List<List<V>> cycles = createCycleDetector().findSimpleCycles();
+		List<CutPoint<V, E>> cutPoints = new ArrayList<>();
+		for (List<V> cycle : cycles) {
+			cutPoints.add(findCutPoint(cycle));
+		}
+		return cutPoints;
+	}
+	
+	public DirectedMultigraph<V, E> cutCycles() {
+		return targetGraph;
+	}
+}
---
+++ javaan-lib/src/test/java/org/javaan/graph/TestMinimumEdgesCycleCut.java
@@ -0,0 +1,58 @@
+package org.javaan.graph;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.List;
+
+import org.javaan.graph.SimpleGraphReader.ObjectProducer;
+import org.jgrapht.graph.DirectedMultigraph;
+import org.jgrapht.graph.DirectedPseudograph;
+import org.junit.Test;
+
+public class TestMinimumEdgesCycleCut {
+	
+	private static final String GRAPH_CONTENT = 
+			"E a b ab1 \n" // first cycle a -> b -> c -> d -> a
+			+ "E b c bc1 \n"
+			+ "E b c bc2 \n"
+			+ "E c d cd1 \n"
+			+ "E c d cd2 \n"
+			+ "E d a da1 \n"
+			+ "E d a da2 \n"
+			+ "E d a da3 \n"
+			+ "E c e ce1 \n" // second cycle c -> e -> b -> c
+			+ "E c e ce2 \n"
+			+ "E c e ce3 \n"
+			+ "E e b eb1";
+
+	private DirectedPseudograph<String, String> createGraph() throws IOException {
+		DirectedPseudograph<String, String> graph = new DirectedPseudograph<String, String>(new UnsupportedEdgeFactory<String, String>());
+		ObjectProducer<String, String> objectProducer = new ObjectProducer<String, String>() {
+			@Override
+			public String createEdge(String source, String target, String edgeLabel) {
+				return source + "-->" + target + ":" + edgeLabel;
+			}
+			
+			@Override
+			public String createVertex(String vertexLabel) {
+				return vertexLabel;
+			}
+		};
+		new SimpleGraphReader<String, String>(graph, objectProducer).readGraph(new StringReader(GRAPH_CONTENT));
+		return graph;
+	}
+
+	@Test
+	public void testFindCutPoints() throws IOException {
+		DirectedMultigraph<String, String> target = new DirectedMultigraph<>(new UnsupportedEdgeFactory<String, String>());
+		List<CutPoint<String, String>> cutPoints = new MinimumEdgesCycleCut<>(createGraph(), target).findCutPoints();
+		assertNotNull(cutPoints);
+		assertEquals(2, cutPoints.size());
+		assertTrue(cutPoints.contains(new CutPoint<String, String>("a", "b")));
+		assertTrue(cutPoints.contains(new CutPoint<String, String>("e", "b")));
+	}
+}
