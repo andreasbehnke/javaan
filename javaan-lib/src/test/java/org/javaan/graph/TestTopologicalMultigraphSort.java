ea76e41f5ea65073a261cfb9a2c7b61db8500fd1
Andreas Behnke <ab@andreasbehnke.com>
2014-01-29 09:34:15 +0100

    implemented algorithm for topological sort of a multigraph

---
+++ javaan-lib/src/test/java/org/javaan/graph/TestTopologicalMultigraphSort.java
@@ -0,0 +1,60 @@
+package org.javaan.graph;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.List;
+
+import org.javaan.graph.SimpleGraphReader.ObjectProducer;
+import org.jgrapht.graph.DirectedMultigraph;
+import org.junit.Test;
+
+public class TestTopologicalMultigraphSort {
+
+	private static final String GRAPH_CONTENT = 
+			"E a b ab1 \n"
+			+ "E a b ab2 \n"
+			+ "E b c bc1 \n"
+			+ "E b c bc2 \n"
+			+ "E c d cd1 \n"
+			+ "E c d cd2 \n"
+			+ "E c e ce1 \n"
+			+ "E c e ce2 \n"
+			+ "E c e ce3 \n"
+			+ "E f g fg1 \n"
+			+ "E g b gb1 \n";
+	
+	private DirectedMultigraph<String, String> createGraph() throws IOException {
+		DirectedMultigraph<String, String> graph = new DirectedMultigraph<String, String>(new UnsupportedEdgeFactory<String, String>());
+		ObjectProducer<String, String> objectProducer = new ObjectProducer<String, String>() {
+			@Override
+			public String createEdge(String source, String target, String edgeLabel) {
+				return source + "-->" + target + ":" + edgeLabel;
+			}
+			
+			@Override
+			public String createVertex(String vertexLabel) {
+				return vertexLabel;
+			}
+		};
+		new SimpleGraphReader<String, String>(graph, objectProducer).readGraph(new StringReader(GRAPH_CONTENT));
+		return graph;
+	}
+			
+	@Test
+	public void testSort() throws IOException {
+		DirectedMultigraph<String, String> graph = createGraph();
+		List<String> sorted = new TopologicalMultigraphSort<>(graph).sort();
+		assertNotNull(sorted);
+		assertEquals(graph.vertexSet().size(), sorted.size());
+		assertEquals("f", sorted.get(0));
+		assertEquals("g", sorted.get(1));
+		assertEquals("a", sorted.get(2));
+		assertEquals("b", sorted.get(3));
+		assertEquals("c", sorted.get(4));
+		assertEquals("e", sorted.get(5));
+		assertEquals("d", sorted.get(6));
+	}
+}
