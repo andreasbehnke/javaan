f57db47d776b72201530aec2d7b44c1074e41af0
Andreas Behnke <ab@andreasbehnke.com>
2014-02-19 08:24:16 +0100

    removed project codeforest. This project has its own repository now to make feature implementation for javaan more easy

--- codeforest/src/main/scripts/codeforest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DIR="$(dirname "$(readlink -f "$0")")"
-ARCH="$(uname -m)"
-NATIVE_LIB_DIR="i386"
-if ([ $ARCH = "x86_64" ] || [ $ARCH = "amd64" ]); then
-  NATIVE_LIB_DIR="amd64"
-fi
-export LD_LIBRARY_PATH=$DIR/$NATIVE_LIB_DIR
-java -jar $DIR/codeforest.jar $1
