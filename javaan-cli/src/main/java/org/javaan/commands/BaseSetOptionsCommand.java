e98af3ba8ee8a3d8a438e4e210379dff3e9712bf
Andreas Behnke <ab@andreasbehnke.com>
2014-02-28 17:34:01 +0100

    introduced base class for set commands

---
+++ javaan-cli/src/main/java/org/javaan/commands/BaseSetOptionsCommand.java
@@ -0,0 +1,77 @@
+package org.javaan.commands;
+
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.javaan.CommandContext;
+import org.javaan.ReturnCodes;
+import org.javaan.Settings;
+
+
+public abstract class BaseSetOptionsCommand extends BaseCommand {
+	
+	protected abstract String getDescriptionFormat();
+
+	@Override
+	public String getDescription() {
+		StringBuilder buffer = new StringBuilder();
+		for (Option option : StandardOptions.PERSISTENT_OPTIONS) {
+			buffer.append(option.getOpt())
+				.append(" (")
+				.append(option.getLongOpt())
+				.append(") ");
+		}
+		return String.format(getDescriptionFormat(), buffer);
+	}
+
+	@Override
+	public Options buildCommandLineOptions(Options options) {
+		return options;
+	}
+	
+
+	private boolean optionExsists(String option) {
+		for (Option optionObj : StandardOptions.PERSISTENT_OPTIONS) {
+			if (optionObj.getOpt().equals(option)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	protected abstract void processOption(Settings settings, String option);
+	
+	private ReturnCodes processOptions(CommandContext commandContext, String[] options) {
+		for (String option : options) {
+			if (!optionExsists(option)) {
+				LOG.error("Unknown option: " + option);
+				return ReturnCodes.errorCommand;
+			}
+		}
+		Settings settings = commandContext.getSettings();
+		for (String option : options) {
+			processOption(settings, option);
+		}
+		return ReturnCodes.ok;
+	}
+	
+	private ReturnCodes listPersistentOptions(CommandContext commandContext) {
+		Settings settings = commandContext.getSettings();
+		System.out.println("The following options are set:");
+		for (Option optionObj : StandardOptions.PERSISTENT_OPTIONS) {
+			if (settings.isOptionEnabled(optionObj.getOpt())) {
+				System.out.println(optionObj.getOpt());
+			}
+		}
+		return ReturnCodes.ok;
+	}
+
+	@Override
+	public ReturnCodes execute(CommandContext commandContext) {
+		String[] options = commandContext.getArguments();
+		if (options.length == 0) {
+			return listPersistentOptions(commandContext);
+		} else {
+			return processOptions(commandContext, options);
+		}
+	}
+}
--- javaan-cli/src/main/java/org/javaan/commands/SetOptions.java
+++ javaan-cli/src/main/java/org/javaan/commands/SetOptions.java
@@ -1,12 +1,8 @@
 package org.javaan.commands;
 
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
-import org.javaan.CommandContext;
-import org.javaan.ReturnCodes;
 import org.javaan.Settings;
 
-public class SetOptions extends BaseCommand {
+public class SetOptions extends BaseSetOptionsCommand {
 
 	private final static String NAME = "set";
 
@@ -24,66 +20,14 @@ public class SetOptions extends BaseCommand {
 	public String getName() {
 		return NAME;
 	}
-
+	
 	@Override
-	public String getDescription() {
-		StringBuilder buffer = new StringBuilder();
-		for (Option option : StandardOptions.PERSISTENT_OPTIONS) {
-			buffer.append(option.getOpt())
-				.append(" (")
-				.append(option.getLongOpt())
-				.append(") ");
-		}
-		return String.format(DESCRIPTION, buffer);
-	}
-
-	@Override
-	public Options buildCommandLineOptions(Options options) {
-		return options;
+	protected String getDescriptionFormat() {
+		return DESCRIPTION;
 	}
 	
-	private boolean optionExsists(String option) {
-		for (Option optionObj : StandardOptions.PERSISTENT_OPTIONS) {
-			if (optionObj.getOpt().equals(option)) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	private ReturnCodes processOptions(CommandContext commandContext, String[] options) {
-		for (String option : options) {
-			if (!optionExsists(option)) {
-				LOG.error("Unknown option: " + option);
-				return ReturnCodes.errorCommand;
-			}
-		}
-		Settings settings = commandContext.getSettings();
-		for (String option : options) {
-			settings.enableOption(option);
-		}
-		return ReturnCodes.ok;
-	}
-	
-	private ReturnCodes listPersistentOptions(CommandContext commandContext) {
-		Settings settings = commandContext.getSettings();
-		System.out.println("The following options are set:");
-		for (Option optionObj : StandardOptions.PERSISTENT_OPTIONS) {
-			if (settings.isOptionEnabled(optionObj.getOpt())) {
-				System.out.println(optionObj.getOpt());
-			}
-		}
-		return ReturnCodes.ok;
-	}
-
 	@Override
-	public ReturnCodes execute(CommandContext commandContext) {
-		String[] options = commandContext.getArguments();
-		if (options.length == 0) {
-			return listPersistentOptions(commandContext);
-		} else {
-			return processOptions(commandContext, options);
-		}
+	protected void processOption(Settings settings, String option) {
+		settings.enableOption(option);
 	}
-
 }
