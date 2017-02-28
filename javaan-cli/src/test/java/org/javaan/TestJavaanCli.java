8a2c334c2649fe71bee8b1b134503bf5fefa8f54
andreasbehnke <ab@andreasbehnke.com>
2017-02-28 22:07:59 +0100

    removed usage of deprecated class Matchers

--- javaan-cli/src/test/java/org/javaan/TestJavaanCli.java
+++ javaan-cli/src/test/java/org/javaan/TestJavaanCli.java
@@ -21,7 +21,7 @@ package org.javaan;
  */
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
