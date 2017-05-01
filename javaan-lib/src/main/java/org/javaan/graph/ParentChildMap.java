3f5d6638333037ea5fcc1c3306c24c7f4c2a56e0
andreasbehnke <ab@andreasbehnke.com>
2017-05-01 16:25:55 +0200

    made ParentChildMap a wrapper around Map<P,List<C>> because otherwise values of lambda expressions need to be copied into internal map structure

--- javaan-lib/src/main/java/org/javaan/graph/ParentChildMap.java
+++ javaan-lib/src/main/java/org/javaan/graph/ParentChildMap.java
@@ -2,14 +2,84 @@ package org.javaan.graph;
 
 import java.util.*;
 
-public class ParentChildMap<P, C> extends HashMap<P, List<C>> {
+public class ParentChildMap<P, C> implements Map<P, List<C>> {
+
+    private final Map<P, List<C>> internalMap;
+
+    public ParentChildMap() {
+        this.internalMap = new HashMap<>();
+    }
+
+    public ParentChildMap(Map<P, List<C>> map) {
+        this.internalMap = map;
+    }
 
     public boolean addChild(P parent, C child) {
-        List<C> children = get(parent);
+        List<C> children = internalMap.get(parent);
         if (children == null) {
             children = new ArrayList<>();
-            put(parent, children);
+            internalMap.put(parent, children);
         }
         return children.add(child);
+    }
+
+    @Override
+    public int size() {
+        return internalMap.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return internalMap.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return internalMap.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return internalMap.containsValue(value);
+    }
+
+    @Override
+    public List<C> get(Object key) {
+        return internalMap.get(key);
+    }
+
+    @Override
+    public List<C> put(P key, List<C> value) {
+        return internalMap.put(key, value);
+    }
+
+    @Override
+    public List<C> remove(Object key) {
+        return internalMap.remove(key);
+    }
+
+    @Override
+    public void putAll(Map<? extends P, ? extends List<C>> m) {
+        internalMap.putAll(m);
+    }
+
+    @Override
+    public void clear() {
+        internalMap.clear();
+    }
+
+    @Override
+    public Set<P> keySet() {
+        return internalMap.keySet();
+    }
+
+    @Override
+    public Collection<List<C>> values() {
+        return internalMap.values();
+    }
+
+    @Override
+    public Set<Entry<P, List<C>>> entrySet() {
+        return internalMap.entrySet();
     }
 }
